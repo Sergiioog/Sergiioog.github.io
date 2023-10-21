5191cdf7d4dd451428a300911808712a1b9dab38
Sergio García <sergiogarcia@iMac-de-Sergio.local>
2023-10-21 11:48:40 +0200

    Solucion js y botones de seleccion de proyectos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var icono_persona = document.querySelector('#icono_persona');
 var icono_ordenador = document.querySelector('#icono_ordenador');
 var icono_libro = document.querySelector('#icono_libro');
 var icono_sobre = document.querySelector('#icono_sobre');
-var icono_github = document.querySelector('#gith');
+var icono_github = document.querySelector('#icono_github');
 var icono_linkedin = document.querySelector('#linked');
 var icono_twitter = document.querySelector('#twt');
 
