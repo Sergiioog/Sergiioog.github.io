bb11cbc7113683549c7a51f039b16474c25bf0f8
Sergiioog <sergiogargor9@gmail.com>
2023-10-22 13:44:21 +0200

    Prueba form de contacto

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+
+//Llamamos a los campos
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "sergiogargor9@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Apellido: $apellido \n";
+$carta .= "Mensaje: $mensaje";
+
+//Enviando mensaje 
+mail($destinatario,$asunto,$carta);
+
+
+
+
+?>
